526e08dabc14e36d1bebe70889f0bd994aceb7be
Zornitsa Gaydarova <zu_91@abv.bg>
2020-08-30 19:10:48 +0300

    Added get user input functionality

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,48 @@
 "use strict";
 class ProjectInput {
     constructor() {
+        this.submitHandler = (event) => {
+            event.preventDefault();
+            const userInput = this.gatherUserInput();
+            if (Array.isArray(userInput)) {
+                const [title, description, people] = userInput;
+                console.log(title, description, people);
+                this.clearInput();
+            }
+        };
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
         const content = document.importNode(this.templateElement.content, true);
         this.element = content.firstElementChild;
         this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
+        this.configure();
         this.attach();
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+    gatherUserInput() {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredTDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+        if (enteredTitle.trim().length === 0 || enteredTDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert('Invalid input!');
+            return;
+        }
+        else {
+            return [enteredTitle, enteredTDescription, +enteredPeople];
+        }
+    }
+    clearInput() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
     }
 }
 const project = new ProjectInput;
--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,63 @@ class ProjectInput {
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
     element: HTMLFormElement
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement
 
         const content = document.importNode(this.templateElement.content, true)
+
         this.element = content.firstElementChild as HTMLFormElement
         this.element.id = 'user-input'
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
+
+        this.configure()
         this.attach()
     }
 
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler)
+    }
+
+    private submitHandler = (event: Event) => {
+        event.preventDefault()
+        const userInput = this.gatherUserInput()
+        if(Array.isArray(userInput)) {
+            const [title, description, people] = userInput
+            console.log(title, description, people);
+            this.clearInput()
+            
+        }
+    }
+
+    private gatherUserInput():[string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value
+        const enteredTDescription = this.descriptionInputElement.value
+        const enteredPeople = this.peopleInputElement.value
+
+        if(enteredTitle.trim().length === 0 || enteredTDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert('Invalid input!')
+            return
+        } else {
+            return [enteredTitle, enteredTDescription, +enteredPeople]
+        }
+    }
+
+    private clearInput() {
+        this.titleInputElement.value = ''
+        this.descriptionInputElement.value = ''
+        this.peopleInputElement.value = ''
     }
 }
 
